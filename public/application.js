12d9e4467756cd90dc08c8d55c9074438bb839d7
dzucconi <mail@damonzucconi.com>
2015-01-25 12:02:39 -0500

    Support for query strings

--- public/application.js
+++ public/application.js
@@ -1,11 +1,32 @@
 (function(exports) {
   'use strict';
 
-  var ndImageUrl, addPaginationLinks, searchComplete, randomElementOf, prepopulate, attachSearch, onLoad;
+  var getParams,
+    ndImageUrl,
+    addPaginationLinks,
+    searchComplete,
+    randomElementOf,
+    prepopulate,
+    attachSearch,
+    onLoad;
+
+  getParams = function() {
+    var qs = location.search.substring(1);
+
+    if (!qs.length) return {};
+
+    return qs.split('&').reduce(function(memo, pair) {
+      var k, v, _ref;
+      _ref = pair.split('='), k = _ref[0], v = _ref[1];
+      memo[k] = decodeURIComponent(v);
+      return memo;
+    }, {});
+  };
 
   ndImageUrl = function(url) {
     return 'http://cdn.northerndelegate.com/' + url.replace(/^http(s?):\/\//, '');
-  }
+  };
+
 
   addPaginationLinks = function() {
     var cursor, curPage, pagesDiv, contentDiv;
@@ -22,7 +43,7 @@
       page = cursor.pages[n];
 
       if (curPage == n) {
-        label = document.createElement('span')
+        label = document.createElement('span');
         label.innerHTML = page.label;
         pagesDiv.appendChild(label);
       } else {
@@ -35,7 +56,7 @@
 
     contentDiv = document.getElementById('content');
     contentDiv.insertBefore(pagesDiv, contentDiv.firstChild);
-  }
+  };
 
   searchComplete = function() {
     if (imageSearch.results && imageSearch.results.length > 0) {
@@ -46,7 +67,7 @@
       results = imageSearch.results;
 
       for (var i = results.length - 1; i >= 0; i--) {
-        var result, imgContainer, link, img;
+        var result, link, img;
 
         result = results[i];
 
@@ -58,7 +79,9 @@
         img.src = ndImageUrl(result.url);
         img.width = result.width;
         img.height = result.height;
-        img.onerror = function() { this.style.display = 'none' };
+        img.onerror = function() {
+          this.style.display = 'none';
+        };
 
         link.appendChild(img);
         contentDiv.appendChild(link);
@@ -66,43 +89,60 @@
 
       addPaginationLinks(imageSearch);
     }
-  }
+  };
 
   randomElementOf = function(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
-  }
+  };
 
   prepopulate = function() {
-    imageSearch.execute(randomElementOf([
-      'Asia',
-      'Africa',
-      'North America',
-      'South America',
-      'Antarctica',
-      'Europe',
-      'Australia'
-    ]));
+    performSearch((
+      params.q ||
+      randomElementOf([
+        'Asia',
+        'Africa',
+        'North America',
+        'South America',
+        'Antarctica',
+        'Europe',
+        'Australia'
+      ])
+    ));
   };
 
   attachSearch = function() {
-    document.getElementById('search').addEventListener('keydown', function(e) {
+    search.addEventListener('keydown', function(e) {
       if (e.which === 13) imageSearch.execute(search.value);
     });
-  }
+    if (params.q) search.value = params.q;
+  };
 
   onLoad = function() {
+    exports.search = document.getElementById('search');
+    exports.params = getParams();
     exports.imageSearch = new google.search.ImageSearch();
+
     imageSearch.setSearchCompleteCallback(this, searchComplete, null);
-
-    imageSearch.setRestriction(google.search.ImageSearch.RESTRICT_IMAGESIZE, google.search.ImageSearch.IMAGESIZE_MEDIUM);
-    imageSearch.setRestriction(google.search.ImageSearch.RESTRICT_FILETYPE, google.search.ImageSearch.FILETYPE_JPG);
-    imageSearch.setRestriction(google.search.ImageSearch.RESTRICT_IMAGETYPE, google.search.ImageSearch.IMAGETYPE_PHOTO);
-
+    [
+      ['RESTRICT_IMAGESIZE', 'IMAGESIZE_MEDIUM'],
+      ['RESTRICT_IMAGETYPE', 'IMAGETYPE_PHOTO'],
+      ['RESTRICT_FILETYPE', 'FILETYPE_JPG']
+    ].forEach(function(restriction) {
+      imageSearch.setRestriction(
+        google.search.ImageSearch[restriction[0]],
+        google.search.ImageSearch[restriction[1]]
+      );
+    });
     imageSearch.setResultSetSize(google.search.Search.LARGE_RESULTSET);
 
     attachSearch();
     prepopulate();
-  }
+  };
+
+  exports.performSearch = function(query) {
+    search.value = query;
+    imageSearch.execute(query);
+  };
 
   google.load('search', '1');
   google.setOnLoadCallback(onLoad);
